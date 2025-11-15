4467d6a1f3b22351667cd7f3952e06ed0e6be6cd
Santiago Isaza Morales <159985540+saisazamo@users.noreply.github.com>
2025-11-15 12:07:13 -0500

    Implement word counting functionality and update migration test

---
+++ homework/src/wordcount.py
@@ -0,0 +1,40 @@
+# obtain a list of files in the input directory
+import os
+
+
+def read_all_lines(input_dir):
+    all_lines = []
+    input_files_list = os.listdir(input_dir)
+    for filename in input_files_list:
+        with open(os.path.join(input_dir, filename)) as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
+
+def main():
+
+    input_files_list = os.listdir('data/input')
+
+    # count the frequency of the words in the files in the input directory
+    counter={}
+    for filename in input_files_list:
+        with open('data/input/'+filename) as f:
+            for l in f:
+                for w in l.split( ):
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+
+    # create the directory output/ if it doesn't exist
+def write_word_count(counter):
+    if not os.path.exists('data/output'):
+        os.makedirs('data/output')
+
+    # save the results using tsv format
+    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+            for key, value in counter.items():
+                # write the key and value to the file
+                f.write(f"{key}\t{value}\n")
+                
+if __name__ == '__main__':
+    main()
+    
