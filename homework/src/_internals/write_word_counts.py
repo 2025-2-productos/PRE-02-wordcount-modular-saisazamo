ae65f5ec6988e5c6e1a8318002ecc9ae04cdc0e1
Santiago Isaza Morales <159985540+saisazamo@users.noreply.github.com>
2025-11-22 11:11:26 -0500

    Code modularized

--- homework/src/_internals/write_word_counts.py
+++ homework/src/_internals/write_word_counts.py
@@ -6,7 +6,7 @@ def write_count_words(counter, output_folder):
         os.makedirs(output_folder)
 
     # write the word counts to a file
-    output_path = os.path.join(output_folder, "results.tsv")
+    output_path = os.path.join(output_folder, "wordcount.tsv")
     with open(output_path, "w", encoding="utf-8") as f:
         for key, value in sorted(counter.items()):
             f.write(f"{key}\t{value}\n")
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,4 +1,5 @@
 # obtain a list of files in the input directory
+import sys
 
 from homework.src._internals.count_words import count_words
 from homework.src._internals.preprocess_lines import preprocess_lines
@@ -8,8 +9,13 @@ from homework.src._internals.write_word_counts import write_count_words
 
 
 def main():
-    input_folder = "data/input/"
-    output_folder = "data/output/"
+    
+    if len(sys.argv) < 3:
+        raise Exception("Usage: python -m homework <input_dir> <output_dir>")
+
+    ## Set input and output folder from command line arguments (sys.argv[0] is the script name)
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
     
     all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
